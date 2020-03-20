802ecc9d037fea6ad0c2f32506549cc6b60e12ae
thomas <thomaselmoleung@gmail.com>
2020-03-20 12:50:34 -0400

    added left, right and reset buttons

--- src/components/EmojiPwd.vue
+++ src/components/EmojiPwd.vue
@@ -1,12 +1,12 @@
 <template>
   <v-sheet class="pa-4">
-    <v-container class="grey lighten-5" style="border-radius:15px">
+    <v-container class="grey lighten-4" style="border-radius:15px">
       <v-row v-for="i in 4" :key="i" no-gutters>
         <v-col v-for="j in 4" :key="j">
           <v-card height="8vh" outlined tile align="center">
             <v-btn
               class="display-1"
-              ripple="true"
+              ripple
               tile
               text
               width="100%"
@@ -14,6 +14,24 @@
             >{{emoji[i == 1? i * j : 4*(i-1) + j]}}</v-btn>
             <!-- <p class="display-1 mb-0">{{emoji[i * j]}}</p> -->
           </v-card>
+        </v-col>
+      </v-row>
+      <v-row align="center" justify="space-between">
+        <v-col class="pb-0 pt-1" cols="auto">
+          <v-btn text>
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-col>
+         <v-col class="pb-0 pt-1" cols="auto">
+          <v-btn text class="caption">
+            <v-icon size="16">mdi-lock-reset</v-icon>
+            Reset
+          </v-btn>
+        </v-col>
+        <v-col class="pb-0 pt-1" cols="auto">
+          <v-btn text>
+            <v-icon>mdi-chevron-right</v-icon>
+          </v-btn>
         </v-col>
       </v-row>
     </v-container>
