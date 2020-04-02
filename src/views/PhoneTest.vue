fd5c41773e61589cd95dc823257f9bdb0131c63f
ChristopheTran <christophetran01@gmail.com>
2020-04-02 01:50:43 -0400

    added export csv to PhoneTest

--- src/views/PhoneTest.vue
+++ src/views/PhoneTest.vue
@@ -20,6 +20,10 @@
           <ul id="log-data">
             <li v-for="(log, index) in logData" :key="index">{{log}}</li>
           </ul>
+           <v-sheet height="2vh"></v-sheet>
+          <div class="exportCSV" align="center">
+            <v-btn color="secondary" @click="exportToCsv('export.csv',logData)">Export to CSV</v-btn>
+          </div>
         </v-card-text>
       </v-card>
     </v-bottom-sheet>
@@ -60,6 +64,55 @@ export default {
     },
     lock(value) {
       this.unlock = value;
+    },
+    exportToCsv(filename, rows) {
+        var processRow = function (row) {
+            var finalVal = '';
+            for (var j = 0; j < row.length; j++) {
+                var innerValue = row[j] === null ? '' : row[j].toString();
+                if (row[j] instanceof Date) {
+                    innerValue = row[j].toLocaleString();
+                }
+                var result = innerValue.replace(/"/g, '""');
+                if (result.search(/("|,|\n)/g) >= 0)
+                    result = '"' + result + '"';
+                if (j > 0)
+                    finalVal += ',';
+                finalVal += result;
+            }
+            return finalVal + '\n';
+        };
+
+        var csvFile = '';
+        var logRows = [];
+        //var result = [];
+        for(var a =0; a < rows.length; a++){
+          var result = rows[a].split(",");
+          console.log(result);
+          logRows.push(result);
+        }
+
+        for (var i = 0; i < logRows.length; i++) {
+            csvFile += processRow(logRows[i]);
+        }
+
+        var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+        if (navigator.msSaveBlob) { // IE 10+
+            navigator.msSaveBlob(blob, filename);
+        } else {
+            var link = document.createElement("a");
+            if (link.download !== undefined) { // feature detection
+                // Browsers that support HTML5 download attribute
+                var url = URL.createObjectURL(blob);
+                link.setAttribute("href", url);
+                link.setAttribute("download", filename);
+                link.style.visibility = 'hidden';
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            }
+        }
+        
     }
   }
 };
